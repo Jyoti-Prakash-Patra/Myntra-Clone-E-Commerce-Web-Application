4bbd1b2b9ec19fad4d634271c050d006e6112c6a
Jyoti <speak2jyotiprakash@gmail.com>
2024-09-21 00:46:22 +0530

    Add hyperlinks JS

---
+++ CommonJS/HyperLinks.js
@@ -0,0 +1,18 @@
+// ----------------- Hyperlinks ------------------------------
+
+document.querySelector('.fa_user').addEventListener('click', function () {
+    window.location.href = "../HomePages/Profile.html"
+})
+
+document.querySelector('.fa-heart').addEventListener('click', function () {
+    window.location.href = "../HomePages/WishList.html"
+})
+document.querySelector(".fa_wishlist").addEventListener('click', function () {
+    window.location.href = "../HomePages/WishList.html"
+})
+document.querySelector('.fa-bag-shopping').addEventListener('click', function () {
+    window.location.href = "../wishlist/cart.html"
+})
+document.querySelector('.fa_cart').addEventListener('click', function () {
+    window.location.href = "../wishlist/cart.html"
+})
