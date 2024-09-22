13a63c13b7325fdf41e8b27c07fcd13f4d429efd
Jyoti <speak2jyotiprakash@gmail.com>
2024-09-22 08:18:59 +0530

    Add Corousel JS for image slidilg

---
+++ CommonJS/Corousel.js
@@ -0,0 +1,9 @@
+// ---------------------------------- Corousel for all slider animation -----------------------------
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 3000);
